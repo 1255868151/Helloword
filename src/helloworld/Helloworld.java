5bca61d818cedb0ec91c1e5182bff9858e30d41c
Administrator <Administrator@8WS4AKRL5L6K4W6>
2018-12-10 11:28:01 +0800

    再次修改

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world ĞŞ¸ÄÒ»ÏÂ");
+		System.out.println("hello world å†æ¬¡ä¿®æ”¹");
 
 	}
 
